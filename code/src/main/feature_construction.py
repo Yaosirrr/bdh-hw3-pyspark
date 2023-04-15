320f613467ab88b2214e1eb3284bf99922201ef6
Yaosirrr <yrj0398@gmail.com>
2023-04-15 17:11:40 -0400

    add dispatch

--- code/src/main/feature_construction.py
+++ code/src/main/feature_construction.py
@@ -2,8 +2,10 @@
 from pyspark.sql import SparkSession
 from pyspark.ml.linalg import Vectors
 from pyspark import RDD
+from multipledispatch import dispatch
 
 from typing import Tuple
+from typing import Set
 from datetime import date
 import sys
 sys.path.append('./')
@@ -19,18 +21,21 @@ sc = spark.sparkContext
 
 FeatureTuple = Tuple[Tuple[str, str], float]
 
+@dispatch(RDD[Diagnostic])
 def constructDiagnosticFeatureTuple(diagnostic: RDD[Diagnostic]) -> RDD[FeatureTuple]:
 
   diag = diagnostic.map(lambda x:((x.patientID, x.code), 1.0))
   diag = diag.reduceByKey(lambda a, b: a + b)
   return diag
 
+@dispatch(RDD[Medication])
 def constructMedicationFeatureTuple(medication: RDD[Medication]) -> RDD[FeatureTuple]:
 
   med = medication.map(lambda x:((x.patientID, x.medicine), 1.0))
   med = med.reduceByKey(lambda a, b: a + b)
   return med
 
+@dispatch(RDD[LabResult])
 def constructLabFeatureTuple(labResult: RDD[LabResult]) -> RDD[FeatureTuple] :
 
   lab_sum = labResult.map(lambda x: ((x.patientID, x.reslultName), x.value)).reduceByKey(lambda a, b: a + b)
@@ -38,10 +43,7 @@ def constructLabFeatureTuple(labResult: RDD[LabResult]) -> RDD[FeatureTuple] :
   lab = lab_sum.join(lab_count).map(lambda x: (x[0], x[1][0] / x[1][1]))
   return lab
 
-'''
-Functions below need to be renamed. Otherwise, they will override
-same-named functions above since Python is fully override.
-'''
+@dispatch(RDD[Diagnostic], Set)
 def constructDiagnosticFeatureTuple(diagnostic, candidateCode):
 
   diag = diagnostic.map(lambda x:((x.patientID, x.code), 1.0))
@@ -49,6 +51,7 @@ def constructDiagnosticFeatureTuple(diagnostic, candidateCode):
   diag_feature = diag.filter(lambda x: x[0][1] in candidateCode)
   return diag_feature
 
+@dispatch(RDD[Medication], Set)
 def constructMedicationFeatureTuple(medication, candidateMedication):
 
   med = medication.map(lambda x:((x.patientID, x.medicine), 1.0))
@@ -56,6 +59,7 @@ def constructMedicationFeatureTuple(medication, candidateMedication):
   med_feature = med.filter(lambda x: x[0][1] in candidateMedication)
   return med_feature
 
+@dispatch(RDD[LabResult], Set)
 def constructLabFeatureTuple(labResult, candidateLab):
   
   lab_sum = labResult.map(lambda x: ((x.patientID, x.reslultName), x.value)).reduceByKey(lambda a, b: a + b)
